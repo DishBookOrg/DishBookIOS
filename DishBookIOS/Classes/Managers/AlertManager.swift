f2130f221e6d28d3d05f6f2bafa554da97ab815c
ddanilyuk <danis.danilyuk@gmail.com>
2021-02-05 19:51:39 +0200

    Update pods, add files to empty folders

---
+++ DishBookIOS/Classes/Managers/AlertManager.swift
@@ -0,0 +1,8 @@
+//
+//  AlertManager.swift
+//  DishBookIOS
+//
+//  Created by Denys Danyliuk on 05.02.2021.
+//
+
+import Foundation
---
+++ DishBookIOS/Classes/Models/Dish.swift
@@ -0,0 +1,8 @@
+//
+//  Dish.swift
+//  DishBookIOS
+//
+//  Created by Denys Danyliuk on 05.02.2021.
+//
+
+import Foundation
