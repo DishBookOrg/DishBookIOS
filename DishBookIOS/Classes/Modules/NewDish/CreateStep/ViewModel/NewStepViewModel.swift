607709aae3e761e7e6b217c31c9519d02b0b1b6f
ddanilyuk <danis.danilyuk@gmail.com>
2021-06-17 23:39:24 +0300

    Add image picker, for steps, add image downloading

--- DishBookIOS/Classes/Modules/NewDish/CreateStep/ViewModel/NewStepViewModel.swift
+++ DishBookIOS/Classes/Modules/NewDish/CreateStep/ViewModel/NewStepViewModel.swift
@@ -5,8 +5,10 @@
 //  Created by Anastasia Holovash on 16.06.2021.
 //
 
-import Foundation
+import UIKit
 import Combine
+import FirebaseStorage
+import CombineFirebaseStorage
 
 final class NewStepViewModel: BaseViewModel {
     
@@ -16,7 +18,57 @@ final class NewStepViewModel: BaseViewModel {
     lazy var didPressBackPublisher = didPressBackSubject.eraseToAnyPublisher()
     
     // MARK: - Subjects
-
+    
     let didPressDoneSubject = PassthroughSubject<IngredientsAndSteps.Step, Never>()
     let didPressBackSubject = PassthroughSubject<Void, Never>()
+    
+    let stepNumber: Int
+    var step: IngredientsAndSteps.Step
+    
+    init(stepNumber: Int, step: IngredientsAndSteps.Step) {
+        
+        self.stepNumber = stepNumber
+        self.step = step
+    }
+    
+    
+}
+
+// MARK: - API
+
+extension NewStepViewModel {
+    
+    func loadImage(_ image: UIImage) {
+        
+        showLoader = true
+        
+        guard let data = image.jpegData(compressionQuality: 0.7) else {
+            return
+        }
+        
+        let newURL = "https://firebasestorage.googleapis.com/v0/b/dishbookapp.appspot.com/o/\(UUID().uuidString).jpeg"
+        let reference = Storage
+            .storage()
+            .reference(forURL: newURL)
+                
+        (reference.putData(data) as AnyPublisher<StorageMetadata, Error>)
+//            .con
+            .sink(receiveCompletion: { [weak self] completion in
+                
+                self?.showLoader = false
+                
+                switch completion {
+                case .finished:
+                    self?.step.stepAttachmentURL = newURL
+                    print("🏁 finished")
+                case .failure(let error):
+                    print(error)
+                }
+            }) { [weak self] metadata in
+                // Success
+                self?.showLoader = false
+                print(metadata)
+            }
+            .store(in: &cancelableSet)
+    }
 }
